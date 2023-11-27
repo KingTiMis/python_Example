1bc3b14dd55b7230a6bc6b446ec15c036a4664b9
hackversion1 <samueltimis20@gmail.com>
2023-11-27 18:50:49 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+a = "Python is cool"
+print(a[0:6])
+
+print(f"{98} Battery street, {'San Francisco'}")
+
+print('Py' 'thon')
+
+print(f"{98} Battery street")
+
+print(a[-4])
+
+print(a[7:])
+
+a = "Python is cool"
+print(a[7:-5])
